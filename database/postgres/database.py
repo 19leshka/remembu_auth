d30e0722012dfa5895951b21392ef1f39176cfba
Alexey Balahanov <alexeybalahanov@gmail.com>
2023-09-08 11:03:02 +0300

    add database and core settings

---
+++ database/postgres/database.py
@@ -0,0 +1,34 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from core.config import settings
+
+url = (
+    f"postgresql+asyncpg://"
+    f"{settings.PG_USER}:"
+    f"{settings.PG_PASS}@"
+    f"{settings.PG_HOST}:"
+    f"{settings.PG_PORT}/"
+    f"{settings.PG_DB}"
+)
+
+
+engine = create_async_engine(url, echo=True)
+Base = declarative_base()
+
+
+AsyncSessionFactory = sessionmaker(
+    class_=AsyncSession, expire_on_commit=False, bind=engine
+)
+
+
+async def get_session() -> AsyncSession:
+    async with AsyncSessionFactory() as session:
+        yield session
+
+
+async def init_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
