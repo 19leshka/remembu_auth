21f35ca628569d947314617c832898d170f5a1bb
Alexey Balahanov <alexeybalahanov@gmail.com>
2023-09-23 17:01:23 +0200

    create user crud action

--- src/crud/crud_user.py
+++ src/crud/crud_user.py
@@ -3,7 +3,7 @@ from typing import Optional
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.core.security import verify_password
+from src.core.security import get_password_hash, verify_password
 from src.crud.base import CRUDBase
 from src.models.user import User
 from src.schemas.user import UserCreate, UserUpdate
@@ -26,6 +26,18 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         if not verify_password(password, user.hashed_password):
             return None
         return user
+
+    async def add(self, session: AsyncSession, *, obj_in: UserCreate) -> User:
+        db_obj = User(
+            email=obj_in.email,
+            hashed_password=get_password_hash(obj_in.password),
+            full_name=obj_in.full_name,
+            is_superuser=obj_in.is_superuser,
+        )
+        session.add(db_obj)
+        await session.commit()
+        await session.refresh(db_obj)
+        return db_obj
 
     async def is_superuser(self, user: User) -> bool:
         return user.is_superuser
