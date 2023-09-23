cfa58b9425d6f1c9922d6ca82adb3bbf56d5166f
Alexey Balahanov <alexeybalahanov@gmail.com>
2023-09-23 17:19:07 +0200

    add forbidden exception

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -5,3 +5,11 @@ from starlette import status
 class DuplicatedEntryError(HTTPException):
     def __init__(self, message: str):
         super().__init__(status_code=status.HTTP_409_CONFLICT, detail=message)
+
+
+class ForbiddenException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+        )
