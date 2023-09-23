4c18d3b2f05fb335154cd8fd1f81214e42e1a466
Alexey Balahanov <alexeybalahanov@gmail.com>
2023-09-23 15:46:02 +0200

    login endpoint

--- src/api/api_v1/endpoints/login.py
+++ src/api/api_v1/endpoints/login.py
@@ -1,3 +1,35 @@
-from fastapi import APIRouter
+from datetime import timedelta
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src import crud
+from src.core import security
+from src.core.config import settings
+from src.database.postgres.database import get_session
+from src.schemas.token import Token
 
 router = APIRouter()
+
+
+@router.post("/login", response_model=Token)
+async def login(
+    db: AsyncSession = Depends(get_session),
+    form_data: OAuth2PasswordRequestForm = Depends(),
+):
+    """
+    OAuth2 compatible token login, get an access token for future requests
+    """
+    user = await crud.user.authenticate(
+        db, email=form_data.username, password=form_data.password
+    )
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    return {
+        "access_token": security.create_access_token(
+            user.id, expires_delta=access_token_expires
+        ),
+        "token_type": "bearer",
+    }
