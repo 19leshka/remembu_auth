c1cc005f0ca6edff2ca52272bd6c734e408f5f67
Alexey Balahanov <alexeybalahanov@gmail.com>
2023-09-29 19:21:50 +0200

    change setting config

--- src/core/config.py
+++ src/core/config.py
@@ -32,7 +32,9 @@ class Settings(BaseSettings):
 
     DOCS_ENVIRONMENT: Tuple[str, ...] = ("local", "staging", "development")
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
 
 
 settings = Settings()
